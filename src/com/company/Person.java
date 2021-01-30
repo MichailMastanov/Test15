6ac8d8b9713ff0785ed9ad1d29e098a792fa9230
MichailMastanov <mixleb030583@gmail.com>
2021-01-30 17:39:28 +0300

    aaa

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.List;
+
 public class Person {
     private int Age;
     private String firstNaeme;
     private String lastName;
+    private List<Car> cars;
+
+    public Person(int age, String firstNaeme, String lastName, List<Car> cars) {
+        Age = age;
+        this.firstNaeme = firstNaeme;
+        this.lastName = lastName;
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
 
     public Person(int age, String firstNaeme, String lastName) {
         Age = age;
